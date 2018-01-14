3bf7476c7b996dc0deb948979d35b729648087ef
Adi Kancherla <findadit@gmail.com>
2018-01-14 02:12:49 -0800

    gg

---
+++ solidity/contracts/PicoloToken.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.4;
+import "../node_modules/zeppelin-solidity/contracts/token/StandardToken.sol";
+import "../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract PicoloToken is StandardToken {
+
+    // metadata
+    string public constant name = "Picolo token";
+    string public constant symbol = "PIC";
+    uint256 public constant decimals = 18;
+    string public version = "1.0";
+
+    // constructor
+    function PicoloToken()
+    {
+      totalSupply = 10000000000;
+      balances[msg.sender] = totalSupply;
+
+    }
+
+}
